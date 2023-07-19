85c21497a2f02c3aa22256d5ecd7d383991cb07a
mikietechie <mzinyoni7@outlook.com>
2023-07-19 23:58:07 +0200

    Pip Freeze

--- app/serializers.py
+++ app/serializers.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 from .models import User, Department, Company, Employee, Employment
 
 
-class UserMiniSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("id", "username", "email", "first_name", "last_name")
+# class UserMiniSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ("id", "username", "email", "first_name", "last_name")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,10 +24,10 @@ class UserAuthSerializer(serializers.ModelSerializer):
         
 
 
-class DepartmentMiniSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Department
-        fields = ("id", "name")
+# class DepartmentMiniSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Department
+#         fields = ("id", "name")
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
@@ -36,10 +36,10 @@ class DepartmentSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CompanyMiniSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = ("id", "name", "registration_number", "contact_person", "number_of_employees")
+# class CompanyMiniSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Company
+#         fields = ("id", "name", "registration_number", "contact_person", "number_of_employees")
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -59,10 +59,10 @@ class CompanySerializer(serializers.ModelSerializer):
         )
 
 
-class EmployeeMiniSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = ("id", "name", "id_number", "contact_phone", "email_address", "trade")
+# class EmployeeMiniSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Employee
+#         fields = ("id", "name", "id_number", "contact_phone", "email_address", "trade")
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -71,16 +71,25 @@ class EmployeeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class EmploymentMiniSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employment
-        fields = ("id", "role", "date_started", "date_left", "data")
+# class EmploymentMiniSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Employment
+#         fields = ("id", "role", "date_started", "date_left", "data")
 
 
 class EmploymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employment
-        fields = "__all__"
+        fields = (
+            "id",
+            "department",
+            "employee",
+            "company",
+            "role",
+            "date_started",
+            "date_left",
+            "duties",
+        )
 
 
 class FileSerializer(serializers.Serializer):
